2521d4852dff05362693e1a5c224cf9763dc362d
Fernanda cruz <fehcruz002@gmail.com>
2025-09-24 21:56:28 -0300

    criando tela de login

--- Senai-notes-angular-jul-25/src/app/app.routes.ts
+++ Senai-notes-angular-jul-25/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginScreen } from './user-module/login-screen/login-screen';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: "login",
+   loadComponent: () => LoginScreen
+
+},
+
+];
