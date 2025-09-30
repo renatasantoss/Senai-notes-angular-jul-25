595a80433f4bef879e50d56dba8e258ee74d209b
Fernanda cruz <fehcruz002@gmail.com>
2025-09-29 21:56:16 -0300

    Atualizando TS

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectorRef, } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-screen',
@@ -9,4 +11,97 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class LoginScreen {
 
+  loginForm: FormGroup;
+
+  emailErrorMessage: string;
+  passwordErrorMessage: string;
+  successStatusMessage: string;
+  errorStatusMessage: string;
+
+  constructor(private fb: FormBuilder, private cd: ChangeDetectorRef) {
+    // Quando a tela iniciar.
+
+    // Inicia o formulário.
+    // Cria o campo obrigatório de email.
+    // Cria o campo obrigatório de senha.
+    this.loginForm = this.fb.group({
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]]
+    });
+
+    // Inicia com uma string vazia
+    this.emailErrorMessage = "";
+    this.passwordErrorMessage = "";
+    this.successStatusMessage = "";
+    this.errorStatusMessage = "";
+
+  }
+
+  async onLoginClick() {
+
+    this.emailErrorMessage = "";
+    this.passwordErrorMessage = "";
+    this.successStatusMessage = "";
+    this.errorStatusMessage = "";
+
+    console.log("Email", this.loginForm.value.email);
+    console.log("Password", this.loginForm.value.password);
+
+    if (this.loginForm.value.email == "") {
+
+      // alert("Preencha o e-mail.");
+      this.emailErrorMessage = "O campo de e-mail é obrigatório.";
+      return;
+
+    }
+
+    if (this.loginForm.value.password == "") {
+
+      this.passwordErrorMessage = "O campo de senha é obrigatório.";
+      return;
+
+    }
+
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+      method: "POST", // Enviar,
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      })
+    });
+
+    console.log("STATUS CODE", response.status);
+
+    // Com base no status, verifique se as credenciais estão corretas e avise o usuário do resultado.
+
+    if (response.status >= 200 && response.status <= 299) {
+
+      this.successStatusMessage = "Login realizado com sucesso!";
+
+      let json = await response.json();
+
+      console.log("JSON", json);
+
+      let meuToken = json.accessToken;
+      let userId = json.user.id;
+
+      localStorage.setItem("meuToken", meuToken);
+      localStorage.setItem("meuId", userId);
+
+      window.location.href = "chat";
+
+    } else {
+
+      this.errorStatusMessage = "Credenciais incorretas.";
+
+    }
+
+    this.cd.detectChanges(); // Forçar uma atualização da tela.
+
+  }
+
 }
+
